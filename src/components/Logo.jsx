263986b8c00a35e742beb04f4990e38bb4db5b19
ashwani1415 <ashwanijaat9820@gmail.com>
2023-12-10 16:03:37 +0530

    logo added

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from "react";
+import LOGO from "../assets/BLOG-FIRM1.png";
 
- const Logo = ({width = "100px"}) => {
+const Logo = ({ width = "100px" }) => {
   return (
-    <div >Logo</div>
-  )
-}
-
+    <div>
+      <img src={LOGO} alt="logo" className="w-20 max-h-14" />
+    </div>
+  );
+};
 
 export default Logo;
